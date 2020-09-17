ab5c356bf22cca0f7b8d74ef2110a93c64a5e395
Peter Phillips <peter.phillips@cumbria.ac.uk>
2020-09-17 13:14:16 +0100

    Testing Travis-based deployment to github pages

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "dpc10ster@gmail.com"
-git config --global user.name "dpc10ster"
+git config --global user.email "peter.phillips@cumbria.ac.uk"
+git config --global user.name "pwep"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
-cp -r ../_book/* ./
+cp -r ../docs/* ./
 git add --all *
-git commit -m"Update the book" || true
+git commit -m "Travis build: Update to book" || true
 git push -q origin gh-pages
